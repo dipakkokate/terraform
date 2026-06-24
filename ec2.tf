4043df5dc408709acd96d7c068e638756a3d2e84
Dipak kokate <dk6939980@gmail.com>
2026-06-24 16:43:27 +0530

    Change EC2 instance type to t3.micro and update block

--- ec2.tf
+++ ec2.tf
@@ -1,14 +1,16 @@
 resource "aws_instance" "devops" {
   ami                    = "ami-0b6d9d3d33ba97d99"
-  instance_type          = "t2.micro"
+  instance_type          = "t3.micro"
   vpc_security_group_ids = ["sg-0228cdd58be6dafc0"]
-  delete_on_termination  = true
-  volume_type            = "gp2"
-  volume_size            = 10
 
+  root_block_device {
+    delete_on_termination = true
+    volume_type           = "gp2"
+    volume_size           = 10
+  }
 
   tags = {
-    Name     = "MyTerraformEC2"
-    purpose  = "webserverinstalled"
+    Name    = "MyTerraformEC2"
+    purpose = "webserverinstalled"
   }
 }
