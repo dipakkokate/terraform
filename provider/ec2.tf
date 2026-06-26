20329a3a515bb959cac5f0378f06472db6fff71f
Dipak kokate <dk6939980@gmail.com>
2026-06-26 16:31:05 +0530

    Add EC2 instance resource configuration

---
+++ provider/ec2.tf
@@ -0,0 +1,19 @@
+resource "aws_instance" "this" {
+  ami           = var.ami_id
+  instance_type = var.instance_types[0]
+
+  vpc_security_group_ids = [
+    var.sg_map["default"]
+  ]
+
+  root_block_device {
+    volume_size           = var.volume_size
+    volume_type           = var.volume_types[0]
+    delete_on_termination = var.delete_on_termination
+  }
+
+  tags = {
+    Name    = var.tags["Name"]
+    Purpose = var.tags["Purpose"]
+  }
+}
